fd59703059ebc79d88db9c00bde552ee104a31a2
Michael Kickham <mfkickham3@gmail.com>
2024-02-20 07:12:18 -0600

    update before work

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,12 +14,16 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jate = await openDB('jate');
+  const jate = await initdb();
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  const jate = await initdb();
+  const transaction = jate.transaction('jate', 'readwrite');
+  const objectStore = transaction.objectStore('jate')
+  const allContent = await objectStore.getAll();
+  return allContent;
 }
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,18 +1,38 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let delayPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+
+    delayPrompt = event;
+
+    butInstall.removeAttribute('disabled')
+});
+
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const start = window.delayPrompt;
-    if (!start){
-        return;
+    if (delayPrompt) {
+        delayPrompt.prompt();
+
+        const userSelect = await delayPrompt.userChoice;
+
+        if (userSelect.outcome === 'accepted') {
+            console.log('Install prompt accepted')
+        } else {
+            console.log('Install prompt declined')
+        }
+
+        delayPrompt = null;
+
+        butInstall.setAttribute('disabled', true);
     }
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.delayPrompt = null;
+    console.log('Application install successful')
 });
